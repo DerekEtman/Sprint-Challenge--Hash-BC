5d120bdf13968e5e2255a9313e2c359adf9bdef0
Derek Etman <dereketman@gmail.com>
2020-02-14 12:23:55 -0800

    + Dont know what I did but it works....

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -2,13 +2,13 @@ import hashlib
 import requests
 
 import sys
-import json
 
 from uuid import uuid4
 
 from timeit import default_timer as timer
 
 import random
+
 
 
 def proof_of_work(last_proof):
@@ -19,44 +19,51 @@ def proof_of_work(last_proof):
     - IE:  last_hash: ...AE9123456, new hash 123456888...
     - p is the previous proof, and p' is the new proof
     - Use the same method to generate SHA-256 hashes as the examples in class
+    - Note:  We are adding the hash of the last proof to a number/nonce for the new proof
     """
 
-    # Problem : We want to generate a new hash and compare the last 6 digits to the last 6 digits of the previous hash
-    # Store previous hash.
-    #  
-
     start = timer()
-    print(f"last Proof: {last_proof}")
 
-    print("Searching for next proof")
-    block_string = json.dumps(last_proof, sort_keys=True).encode()
-    print(f"block_string: {block_string}")
+    proof = random.randint(1000000, 999999999)
+    print("Checking")
 
-    proof = 0
+    encoded_last_p = f"{last_proof}".encode()
+    last_hash = hashlib.sha256(encoded_last_p).hexdigest()
 
+    counter = 0
 
-    while not valid_proof(block_string, proof):
-        proof += 1
-    return proof
+    while not valid_proof(last_hash, proof):
+        proof = random.randint(1000000, 999999999)
 
+        counter += 1
+
+        if counter == 1000000:
+            print(f"currently at: {counter}")
+            if requests.get(url=node + "/last_proof").json()["proof"] != last_proof:
+                print(f"already taken")
+                counter = 0
+                return None
+            counter = 0
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
+    return proof
 
 
 def valid_proof(last_hash, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
-    the hash of the last proof match the first six characters of the hash
-    of the new proof?
-
-    IE:  last_hash: ...AE9123456, new hash 123456E88...
+    the hash of the last proof match the first six characters of the proof?
+    IE:  last_hash: ...AE9123456, new hash 123456888...
     """
 
-    print(f"valid_proof: \n last_hash: {last_hash} \n proof: {proof}")
-    guess = f"{last_hash}{proof}".encode()
-    guess_hash = hashlib.sha256(proof).hexdigest
+    # taking in proof and encoding it.
+    encoded_new = f"{proof}".encode()
 
-    return guess_hash[:6] == "000000"
+    # Converting encoded proof and changing it to sha256
+    new_hash = hashlib.sha256(encoded_new).hexdigest()
+
+    # Comparing first 6 chars to new_hash last 6 chars 
+    return last_hash[-6:] == new_hash[:6]
 
 
 if __name__ == '__main__':
@@ -84,13 +91,15 @@ if __name__ == '__main__':
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
 
-        post_data = {"proof": new_proof,
-                     "id": id}
+        if new_proof is not None:
+            post_data = {"proof": new_proof,
+                         "id": id}
 
-        r = requests.post(url=node + "/mine", json=post_data)
-        data = r.json()
-        if data.get('message') == 'New Block Forged':
-            coins_mined += 1
-            print("Total coins mined: " + str(coins_mined))
-        else:
-            print(data.get('message'))
+            r = requests.post(url=node + "/mine", json=post_data)
+            data = r.json()
+            print(data)
+            if data.get('message') == 'New Block Forged':
+                coins_mined += 1
+                print("Total coins mined: " + str(coins_mined))
+            else:
+                print(data.get('message'))
